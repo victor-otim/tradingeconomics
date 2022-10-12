67c94cd8e62a6d239a75dc48da97a9f7736d55a0
Victor Otim <sirotim@gmail.com>
2022-10-13 01:31:18 +0300

    Update historicalIndicators.php
    Simple PHP page showing percentage population growth rate for Sweden Vs Thailand

--- PHP/historicalIndicators.php
+++ PHP/historicalIndicators.php
@@ -2,21 +2,21 @@
  
 $urls = array(
     //To get a specific country and indicator
-    'https://api.tradingeconomics.com/historical/country/united%20states/indicator/gdp',
+    #'https://api.tradingeconomics.com/historical/country/sweden/indicator/gdp',
     //To get only a start date for your historical data
-    'https://api.tradingeconomics.com/historical/country/united%20states/indicator/gdp/2013-01-01',
+    #'https://api.tradingeconomics.com/historical/country/united%20states/indicator/gdp/2013-01-01',
     //To get a start date and end date
-    'https://api.tradingeconomics.com/historical/country/united%20states/indicator/gdp/2015-01-01/2015-12-31',
+    #'https://api.tradingeconomics.com/historical/country/united%20states/indicator/gdp/2015-01-01/2015-12-31',
     //To get multiple indicators for specific country
-    'https://api.tradingeconomics.com/historical/country/united%20states/indicator/gdp,population',
+    #'https://api.tradingeconomics.com/historical/country/united%20states/indicator/gdp,population',
     //To get specific indicator for multiple countries
-    'https://api.tradingeconomics.com/historical/country/united%20states,china/indicator/gdp',
+    'https://api.tradingeconomics.com/historical/country/sweden,thailand/indicator/population',
     //To get historical data by ticker
-    'https://api.tradingeconomics.com/historical/ticker/USURTOT/2015-03-01',
+    #'https://api.tradingeconomics.com/historical/ticker/USURTOT/2015-03-01',
 ); 
 $headers = array(
     "Accept: application/xml",
-    "Authorization: Client guest:guest"
+    "Authorization: Client ibilj93f487bmu4:ir15nspgjv4p714"
 );
 //An array that will contain all of the information
 //relating to each request.
@@ -58,8 +58,142 @@ foreach($requests as $k => $request){
 }
 //Close the multi handle.
 curl_multi_close($mh);
- 
-//var_dump the $requests array 
-var_dump($requests);
+
+
+$data = json_decode($requests[0]['content'], true);
+
+$country = array();
+
+$years = array();
+
+$years_ctr = 0;
+$start_year = '2010';
+
+foreach ($data as $population):
+
+    $this_year = date('Y', strtotime($population['DateTime']));
+    $country_name = $population['Country'];
+
+    if($this_year >= $start_year && !empty($population['Category'])):
+
+        $this_abs_variance = $population['Value'] - $prev_val[$country_name];
+        $this_relative_variance = $this_abs_variance / $prev_val[$country_name];
+
+        $country[$country_name]['name'] = $country_name;
+        $country[$country_name]['data'][] = round( $this_relative_variance * 100, 2);
+        $years[$this_year] = $this_year;
+
+        $prev_val[$country_name] = $population['Value'];
+
+        elseif($start_year - $this_year == 1 && !empty($population['Category'])):
+
+        $prev_val[$country_name] = $population['Value'];
+
+    endif;
+
+
+endforeach;
 
 ?>
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+
+    <style>
+        .highcharts-figure,
+        .highcharts-data-table table {
+            min-width: 360px;
+            max-width: 800px;
+            margin: 1em auto;
+        }
+
+        .highcharts-data-table table {
+            font-family: Verdana, sans-serif;
+            border-collapse: collapse;
+            border: 1px solid #ebebeb;
+            margin: 10px auto;
+            text-align: center;
+            width: 100%;
+            max-width: 500px;
+        }
+
+        .highcharts-data-table caption {
+            padding: 1em 0;
+            font-size: 1.2em;
+            color: #555;
+        }
+
+        .highcharts-data-table th {
+            font-weight: 600;
+            padding: 0.5em;
+        }
+
+        .highcharts-data-table td,
+        .highcharts-data-table th,
+        .highcharts-data-table caption {
+            padding: 0.5em;
+        }
+
+        .highcharts-data-table thead tr,
+        .highcharts-data-table tr:nth-child(even) {
+            background: #f8f8f8;
+        }
+
+        .highcharts-data-table tr:hover {
+            background: #f1f7ff;
+        }
+    </style>
+</head>
+<body style="background-color:#f2f2f2; font-family: 'Montserrat', sans-serif;">
+<h1 style="text-align:center; color:#000;">Trading Economics</h1>
+<h2 style="text-align:center; color:#000;">Population annual growth rate Sweden Vs Thailand</h2>
+<br>
+<figure class="highcharts-figure">
+    <div id="container"></div>
+    <p class="highcharts-description">
+        This chart compares annual population growth rate between Sweden and Thailand.
+    </p>
+</figure>
+</body>
+<script type="application/javascript">
+    var series = [];
+
+    <?foreach ($country as $key=>$val):?>
+        series.push(<?=json_encode($val)?>);
+    <?endforeach;?>
+    Highcharts.chart('container', {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Annual Population Growth (%)'
+        },
+        subtitle: {
+            text: 'Source: ' +
+                '<a href="https://api.tradingeconomics.com/historical/country/sweden,thailand/indicator/population" ' +
+                'target="_blank">TradingEconomics.com</a>'
+        },
+        xAxis: {
+            categories: [<?=implode(',', $years)?>]
+        },
+        yAxis: {
+            title: {
+                text: 'Population growth rate (%)'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: series
+    });
+</script>
+</html>
